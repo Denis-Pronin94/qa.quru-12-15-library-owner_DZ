cf90add880a2854ceb2896c04dfecbd3664a90a9
dddpr <ddd.pronin@yandex.ru>
2022-06-08 16:09:41 +0300

    Fix

--- src/test/java/tests/WebTest.java
+++ src/test/java/tests/WebTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.title;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebTest extends TestBase {
@@ -11,7 +12,8 @@ public class WebTest extends TestBase {
     @Test
     public void testGithubTitle() {
 
-        open("https://github.com");
+        step("Открыть GitHub", () ->
+             open("https://github.com"));
 
         assertEquals("GitHub: Where the world builds software · GitHub", title());
     }
